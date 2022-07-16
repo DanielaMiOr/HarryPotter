deb663063a8e3cd7ae36a11c0328a1c8ff832a33
Daniela Miñon <danielamactriz@gmail.com>
2022-07-16 16:18:07 -0500

    advancement of structures

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ import Home from './components/Home';
 function App() {
   return (
     <div className="App">
+      <section className="containerNav">
+                <section className="containerBtnMenu">
+                    <button className="btnFavorites">FAVORITOS</button>
+                    <button className="btnAdd">AGREGAR
+                    </button>
+                </section>
+
+                <section className="logo">
+                    <img
+                        className="imgLogo"
+                        src={require("./img/logoHP.png")}
+                        alt="imgLogo"
+                    />
+                </section>
+            </section>
 <Home/>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,17 @@
 
-function Home(){
-    return(
+function Home() {
+    return (
         <div>
-            <section> 
-                <button className="btnFavorites">FAVORITOS</button>
-                <button className="btnAdd">AGREGAR 
-                </button>
-            </section>
+            
             <section>
-                Logo
-                <p className="pInstruction">Selecciona tu filtro</p>
-                <section>
                 <button className="btonStudents">Estudiantes</button>
                 <button className="btonStaf">STAFF</button>
-                </section>
-                <section className="containerCards">
-
-                </section>
+            </section>
+            <section className="containerCards">
 
             </section>
+
+
         </div>
     )
 }
